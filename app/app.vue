de0812c0fd4ba283f198e667d7c1e193dd431fde
kira <ahmed-abotaleb@outlook.com>
2025-09-01 22:16:16 +0300

    Tl 11 (#50)
    * setup tailwind and daisyui
    
    * add landing page
    
    * add landing page

--- app/app.vue
+++ app/app.vue
@@ -2,14 +2,9 @@
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold text-red-500 underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-primary">
-      <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
 <style>
