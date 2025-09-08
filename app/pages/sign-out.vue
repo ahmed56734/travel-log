2da8ec2b76302eabff87ac2d1c620e4324c6c671
ahmed <ahmed-abotaleb@outlook.com>
2025-09-08 03:44:27 +0300

    enhance auth-button with user dropdown and add sign-out flow

---
+++ app/pages/sign-out.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div
+    class="card bg-base-300 container mx-auto mt-4 flex min-h-72 flex-col justify-center items-center gap-4 text-center"
+  >
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
+
+<style scoped></style>
